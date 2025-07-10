22dc5b4cf5e1b86d5462d844e4ed0efc724efbb2
Manu Shree Tripathi <146372684+manutripathi28@users.noreply.github.com>
2025-07-10 22:43:13 +0530

    Create Basic.java

---
+++ Basic.java
@@ -0,0 +1,452 @@
+    
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+//   public class Basic{
+//      public static void main(String[] args) {
+//          System.out.println("Hello\nManushree");
+
+// import javax.sound.sampled.SourceDataLine++
+         
+         //MATHEMATICAL OPERATION 
+
+         // int num1=10;
+          // int num2=20;
+          // int num3=12;
+          // int num4=55;
+          //int c=num1+num2;
+          //int d=num2/num1;
+          //System.out.println(d);
+          // int a=-num1+num2*num3;
+          // int m=(num2+num3)%num1;
+          // int b=num1+(-num2*num3/num4);
+          // System.out.println(a);
+          // System.out.println(m);
+          // System.out.println(b);
+
+ //TAKING INPUT FROM USER
+            //creating a scanner object to read input from the user
+         //   Scanner ab= new Scanner(System.in);
+         //   //prompt the user to input the first number
+         //   System.out.println("Enter first no. :");
+         //    //read and store the first number 
+         //    int num1= ab.nextInt();
+
+         //    //prompt the user to input second number
+         //    System.out.println("enter the second no. :");
+         //       //read and store the second number
+         //       int num2=ab.nextInt();
+                 
+ //performing the mathematical performance of given number
+         //      int c=num1*num2;
+         //       int x=num1=num2;
+         //       int t=num1-num2;
+         //       int g=num2/num1;
+         //       int h=num2%num1;
+
+         //       System.out.println(c);
+         //       System.out.println(x);
+         //       System.out.println(t);
+         //       System.out.println(g);
+         //       System.out.println(h);
+
+         
+//takes a number as input and prints its multiplication tables up to 10         
+         
+         
+        //  Scanner ab=new Scanner(System.in);
+        //  System.out.println("Enter a number: ");
+         
+        //  int num= ab.nextInt();
+        //   for(int i=1; i<=10;i++){
+        //     System.out.println(num + "*" + i + "=" + num*i);
+
+        //   }
+//finding factorail
+          // int num=ab.nextInt();
+          // int fact= 1;
+          // for(int i=1; i<=num; i++){
+          //   fact=fact*i;
+          //   System.out.println(fact);
+          // }
+// calculation
+          // System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+           
+          // double result= 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+          // System.out.println(result);
+    //  }
+    
+    // }
+
+
+ //   print area and parameter of the circle
+      
+       // public class Infosys{
+         //   public static void main(String[] args) {
+                  // Scanner ab= new Scanner(System.in);
+                  // System.out.println("enter the radius");
+
+                  // double radius=ab.nextDouble();
+                  // double perimeter=2*Math.PI*radius;
+                  // double area=Math.PI*radius*radius;
+
+                  // System.out.println("Perimeter is = " + perimeter);
+                  // System.out.println("Area is = " + area);
+                  
+
+// takes three number as input to calculate and print the average 
+    
+                  // Scanner ab= new Scanner(System.in);
+                  // System.out.println("enter the first number");
+
+                  // int g=ab.nextInt();
+
+                  // System.out.println("enter the second number:");
+                  // int f=ab.nextInt();
+
+                  // System.out.println("enter the third number:");
+                  // int h=ab.nextInt();
+
+                  // double j=(g+f+h)/3;
+
+                  // System.out.println("average is:" + j);
+         
+         
+//   area and perimeter of rectangle
+
+                  // Scanner ap=new Scanner(System.in);
+                  // System.out.println("enter the width:");
+                  // double w= ap.nextDouble();
+
+                  // System.out.println("enter the height:");
+                  // double h= ap.nextDouble();
+
+                  // double area= w*h;
+                  // double perimeter= 2*(w+h);
+                  // System.out.println(area);
+                  // System.out.println(perimeter);
+
+
+
+  // swapping of two numbers
+                  
+            //      Scanner sw= new Scanner(System.in);
+            //      System.out.println("enter first number"); 
+            //       int a=sw.nextInt();
+            //        System.out.println("enterr the second number:");
+            //        int b=sw.nextInt();
+
+            //        //print the values before swapping
+            //        System.out.println("Before swapping: a,b =" +a+"," +b);
+
+            //        //perform the swap using a temporary variable
+            //        int vary=a;
+            //        a=b;
+            //        b=vary;
+
+            //        //print values after swapping
+            //        System.out.println("after swapping: a,b =" +a+ "," +b);
+
+
+      //  ADDING TWO BINARY NUBERS
+
+      //       Scanner sc=new Scanner(System.in);
+      //       System.out.println("enter the first binary number: ");
+      //       String binary1= sc.nextLine();
+
+      //       System.out.println("enter the second binary number: ");
+      //       String binary2= sc.nextLine();
+
+      //       //convert binary string to decimal string
+
+      //       int num1= Integer.parseInt(binary1,2);
+      //       int num2=Integer.parseInt(binary2,2);
+      //       //Integer.ParseInt(String,int radix) it is used for converting number represented as string in different bases     
+      //       // int radix represent base of string jisko convert karna hai
+            
+      //      // int sum= num1+num2;
+
+      //  //MULTIPLY TWO BINARY DIGITS
+      //          int mul=num1*num2;
+      //       // convert the sum back to binary
+      //       String binaryMul= Integer.toBinaryString(mul);
+      //       //String binarySum= Integer.toBinaryString(sum);
+      //       // Integer.toBinaryString(sum) this helps to convert decimal to binary
+      //       //(sum) result of adding two decimal values that were origannly binary strings
+
+      //      // System.out.println("the sum of two binary number is: " + binarySum);
+      
+      //      System.out.println("the multiplication of two number is: " + binaryMul);
+      
+      
+    // COMPARE OF TWO NUMBERS 
+
+              // Scanner sc= new Scanner(System.in);
+              // System.out.println("enter the first number:");
+
+              // int num1= sc.nextInt();
+
+              // System.out.println("enter the second number:");
+              // int num2=sc.nextInt();
+
+              // //compare the two numbers
+
+              // if(num1>num2){
+              //   System.out.println("num1 is greater than num2: ");
+              // }
+              // else if(num1<num2){
+              //   System.out.println("num2 is grater than num1: ");
+              // }
+              // else{
+              //   System.out.println("both numbers are equal: ");
+              // }
+      
+      
+      //REVERSE STRING
+
+        //    Scanner abc=new Scanner(System.in);
+        //    System.out.println("enter the string: "); 
+
+        //    char[] letters= abc.nextLine().toCharArray();
+        //    //abc.nextLine()-- it reads the intire input from user
+        //    //.toCharArray()-- it converts the input to charater array.
+        //    //this allows each character of the string to be accessed individually
+
+        //    System.out.println("Reverse string: ");
+
+        //    for(int i= letters.length -1; i>=0; i--){
+        // //this for loop starts from the last character of the array(letters.length-1) and decrement(i--) until it reaches the first character(i>=0)
+
+        //   System.out.print(letters[i]);
+        //   }
+               // System.out.println("\n");
+
+
+//count the letters, space, numbers and other characters
+
+        //         String test= "i want to go to another universe, 19283, FHJ?? kj: 12.33";
+                
+        //          count(test);
+        //          // call the count count method to analyze the characters in the test string
+        //     }
+
+        //     public static void count(String x){
+        //         char[] ch= x.toCharArray();
+        //         //convert the input string to a character array
+
+        //         int letter=0;
+        //         int space= 0;
+        //         int num=0;
+        //         int other=0;
+        //         // above we initialize counters for letters, spaces, numbers, and others characters
+               
+                
+        //         for(int i=0; i<x.length(); i++){
+        //         //iterate through the character array to categorize characters   
+                       
+        //             //check if the character is letter
+        //                 if(Character.isLetter(ch[i])){
+        //                         letter++;
+        //                 }
+        //                 //check if the character ia s digit
+        //                 else if(Character.isLetter(ch[i])){
+        //                         num++;
+        //                 }
+        //                 //check if the character is space
+        //                 if(Character.isSpaceChar(ch[i])){
+        //                         space++;
+        //                 }
+        //                 //character falls into "other' category if none of the condition are met
+        //                 else{
+        //                         other++;
+        //                 }
+        //         }
+        
+        //         System.out.println(" the string is: i want to go to another universe, 19283, FHJ?? kj: 12.33");
+
+        //         //display the count of letters, spaces, numbers, and other characters
+
+        //         System.out.println("letter: " +letter);
+        //         System.out.println("number: " + num);
+        //         System.out.println("space: " +space );
+        //         System.out.println("other: " +other);
+
+
+
+
+// print odd  and even numberfrom 1 to 99 
+                // for(int i=1; i<=100; i++){
+                //         //check if the number is odd 
+                //        // if(i % 2 !=0){
+                //        if(i % 2==0){
+                //                  System.out.println(i);
+                //         }
+                // }
+                
+//CHECK IF NUMBER IS EVEN OR ODD
+                //        Scanner sc=new Scanner(System.in);
+                //        System.out.println("enter the number: ");
+                //        int num1=sc.nextInt();
+
+                //         if(num1 %2 == 0){
+                //                 System.out.println("even number: ");
+                //         }
+                //         else{
+                //                 System.out.println("odd number: ");
+                //         }
+
+
+
+
+  //     CONVERT THE SECONDS TO HOURS,MINUTES, SECONDS         
+                // Scanner scanner = new Scanner(System.in);
+        
+                // // Input: Total seconds
+                // System.out.print("Enter the number of seconds: ");
+                // int totalSeconds = scanner.nextInt();
+
+                // // Calculating hours, minutes, and seconds
+                // int hours = totalSeconds / 3600;  // 1 hour = 3600 seconds
+                // int remainingSeconds = totalSeconds % 3600;
+                // int minutes = remainingSeconds / 60; // 1 minute = 60 seconds
+                // int seconds = remainingSeconds % 60;
+
+                // // Output: Display the results
+                // System.out.println(totalSeconds + " seconds is equivalent to:");
+                // System.out.println(hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
+         // }
+//}
+
+
+
+// //CONDITIONAL STATEMENT
+
+//     public class Infosys{
+//         public static void main(String[] args){
+//                  int age=20;
+
+//                 //  if(age>=18)
+//                 //      System.out.println("you can vote now"); // agr hum curly braces nai lagaenge toh sirf ye line as a conditional chelgi if ke baad jab tak condition true hogi
+//                 //      //aur iske baad ki saari lines chlegi hi chlegi chahe condition true ho chahe false 
+//                 // System.out.println("you cannot vote");
+// //IF/ELSE STATEMENTS
+
+//                 if (age>=19){
+//                         System.out.println("you can vote");
+//                 }
+//                 else{
+//                         System.out.println("you cannot vote");
+//                 }
+//         }
+//     }
+
+
+
+//BASICS OF ARRAY
+
+public class Infosys{
+        public static void main(String[] args) {
+        //         // int age[];  //declaration 
+        //         // age = new int[5]; //allocation
+
+        //// Pehle age[] ko declare kiya — yeh Java ko batata hai ki ye ek int array hoga.
+         ////Uske baad new int[5] likh kar memory allocate ki gayi with 5 elements.
+        ////Lekin koi values nahi di gayi — to Java har element ko default 0 se fill karega.
+        /// yaha pe values manually daalni padegi, warna sab element 0 se fill krega
+        
+
+
+        //         int age[]= new int[5]; 
+        // // both the code are same but in first we have done declaration and allocation in different 
+        // // and in second one both alllocation and declaration done in the same line
+        //         age[0]=5;
+        //         age[1]=10;
+        //       System.out.println(age[0]);
+        //       System.out.println(age[1]);
+        //       System.out.println(age[2]);   
+             
+        ////yaha hum length nikal rhe hai
+        //       System.out.println("the lenght of the array is:" + age.length ); 
+
+
+
+////  yaha pe  neeche initializaton kr rhe hai array ka jaha pe hume size aur value dono pata hai
+
+
+             int marks[]={25, 28, 22, 24,27, 26};
+//        // //Yahan declaration + initialization ek hi line mein ho raha hai.
+//         ////Array ban gaya, aur usme values bhi daal di gayi.
+//         /////Memory bhi allocate hui, aur values bhi assign ho gayi.
+                
+//         System.out.println(marks[0]);
+//         System.out.println(marks[1]);
+//         System.out.println(marks[2]);
+
+
+//ITERATION IN ARRAY 
+
+        String names[]={"aditi", "kanishka", "amaan", "manushree"};
+                for (int i=0; i < names.length; i++) {
+                        System.out.println("name is: " + names[i]);
+                }   
+  //FOR-EACH LOOP ITERATION IN AARAY
+  // it is mostly used in arrays
+
+         int ages[]={24,25,26,28,29};
+
+                for( int age: ages ){
+                        System.out.println("for each "+ age);
+                }
+
+//TAKING THE SIZE AND VALUES OF ARRAY FROM USER
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Kitne elements chahiye? ");
+        int size = sc.nextInt();          // runtime pe size mil gaya
+
+        int[] arr = new int[size];        // array bana liya
+
+        System.out.println("Ab values daalo:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();        // values input le li
+        }
+
+        System.out.print("Array: ");
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+}
+     
+
+
+        
+
+
+
+
+
+
+            
+
+
+      
+       
+
+        
+
+        
+
+          
+         
+
+    
+    
+    
+    
+    
+        
+     
+      
+        
